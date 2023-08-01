9c9adcc5f0200148372f1d63041df4bd9e40c755
MehediHasan06 <mehedihasanmasud06@gmail.com>
2023-08-01 09:45:14 +0600

    rootlayout with typescript initialized

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,22 @@
+import RootLayout from '@/components/layouts/RootLayout';
 import { Button } from 'antd';
+import { ReactElement } from 'react';
+import { NextPageWithLayout } from './_app';
 
 
-export default function Home() {
+const Home: NextPageWithLayout = () => {
   return (
     <main>
       <h1>Hello Next!!!</h1>
       <Button type="primary">Primary Button</Button>
     </main>
+  )
+};
+
+export default Home;
+
+Home.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <RootLayout>{page}</RootLayout>
   )
 }
